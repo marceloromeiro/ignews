fa2ca43672fb960414bbc50a6587b3089e7477eb
Marcelo de Souza Romeiro <marcelo.romeiro@zema.com>
2021-11-23 18:56:10 -0300

    third commit

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,19 @@
 import { FaGithub } from "react-icons/fa";
 import styles from "./styles.module.scss";
 import { FiX } from 'react-icons/fi'
+import { signIn, useSession } from 'next-auth/client'
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
       Marcelo
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button type="button" className={styles.signInButton} onClick={()=> signIn('github')}>
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
